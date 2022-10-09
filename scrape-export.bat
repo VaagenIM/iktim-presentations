63f048a7620cbba553b82e8c0392ea04a967f335
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-10-09 16:26:17 +0200

    :rocket: Rename Files

--- scrape-export.bat
+++ scrape-export.bat
@@ -1,8 +1,8 @@
 @echo off
-echo Drag and drop a .html file from "original-slides", generated from slides.com
+echo Drag and drop a .html file from the "originals" folder, generated via https://slides.com
 echo Hit enter to confirm.
 pause
-python "%~p0.github\scrape-slides.py" "%~p0original-slides\%~n1.html" "%~p0docs\22-23\%~n1\index.html"
+python "%~p0.github\scrape-slides.py" "%~p0originals\%~n1.html" "%~p0docs\22-23\%~n1\index.html"
 echo Completed
 echo Please verify that everything looks okay in %~p0docs\22-23\%~n1\ before committing changes
 pause
